252f74c76e31be48444553383e011527778b0ef2
Scott W Harden <swharden@gmail.com>
2018-09-02 20:10:44 -0400

    split dir view

---
+++ src/SplitDirView/SplitDirView/UserControl1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace SplitDirView
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem15 = new System.Windows.Forms.ListViewItem("C:\\");
+            System.Windows.Forms.ListViewItem listViewItem16 = new System.Windows.Forms.ListViewItem(" test");
+            System.Windows.Forms.ListViewItem listViewItem21 = new System.Windows.Forms.ListViewItem("  folder");
+            System.Windows.Forms.ListViewItem listViewItem22 = new System.Windows.Forms.ListViewItem("   path");
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl1));
+            System.Windows.Forms.ListViewItem listViewItem23 = new System.Windows.Forms.ListViewItem("folder", "Folder_16x.png");
+            System.Windows.Forms.ListViewItem listViewItem24 = new System.Windows.Forms.ListViewItem("file", "RFile_16x.png");
+            this.btnSetFolder = new System.Windows.Forms.Button();
+            this.lvFolders = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.lvFiles = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnCopyFolder = new System.Windows.Forms.Button();
+            this.btnLaunchFolder = new System.Windows.Forms.Button();
+            this.btnCopyFile = new System.Windows.Forms.Button();
+            this.btnLaunchFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSetFolder
+            // 
+            this.btnSetFolder.Location = new System.Drawing.Point(27, 10);
+            this.btnSetFolder.Name = "btnSetFolder";
+            this.btnSetFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSetFolder.TabIndex = 2;
+            this.btnSetFolder.Text = "Set Folder";
+            this.btnSetFolder.UseVisualStyleBackColor = true;
+            this.btnSetFolder.Click += new System.EventHandler(this.btnSetFolder_Click);
+            // 
+            // lvFolders
+            // 
+            this.lvFolders.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvFolders.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lvFolders.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem15,
+            listViewItem16,
+            listViewItem21,
+            listViewItem22});
+            this.lvFolders.Location = new System.Drawing.Point(27, 39);
+            this.lvFolders.Name = "lvFolders";
+            this.lvFolders.Size = new System.Drawing.Size(285, 227);
+            this.lvFolders.TabIndex = 3;
+            this.lvFolders.UseCompatibleStateImageBehavior = false;
+            this.lvFolders.View = System.Windows.Forms.View.List;
+            this.lvFolders.SelectedIndexChanged += new System.EventHandler(this.lvFolders_SelectedIndexChanged);
+            this.lvFolders.DoubleClick += new System.EventHandler(this.lvFolders_DoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Folder_16x.png");
+            this.imageList1.Images.SetKeyName(1, "FolderOpen_16x.png");
+            this.imageList1.Images.SetKeyName(2, "RFile_16x.png");
+            // 
+            // lvFiles
+            // 
+            this.lvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.lvFiles.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvFiles.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lvFiles.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem23,
+            listViewItem24});
+            this.lvFiles.Location = new System.Drawing.Point(27, 272);
+            this.lvFiles.Name = "lvFiles";
+            this.lvFiles.Size = new System.Drawing.Size(285, 397);
+            this.lvFiles.SmallImageList = this.imageList1;
+            this.lvFiles.TabIndex = 4;
+            this.lvFiles.UseCompatibleStateImageBehavior = false;
+            this.lvFiles.View = System.Windows.Forms.View.Details;
+            this.lvFiles.SelectedIndexChanged += new System.EventHandler(this.lvFiles_SelectedIndexChanged);
+            this.lvFiles.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lvFiles_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Width = 123;
+            // 
+            // btnCopyFolder
+            // 
+            this.btnCopyFolder.Location = new System.Drawing.Point(108, 10);
+            this.btnCopyFolder.Name = "btnCopyFolder";
+            this.btnCopyFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnCopyFolder.TabIndex = 5;
+            this.btnCopyFolder.Text = "Copy";
+            this.btnCopyFolder.UseVisualStyleBackColor = true;
+            this.btnCopyFolder.Click += new System.EventHandler(this.btnCopyFolder_Click);
+            // 
+            // btnLaunchFolder
+            // 
+            this.btnLaunchFolder.Location = new System.Drawing.Point(189, 10);
+            this.btnLaunchFolder.Name = "btnLaunchFolder";
+            this.btnLaunchFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnLaunchFolder.TabIndex = 6;
+            this.btnLaunchFolder.Text = "Launch Folder";
+            this.btnLaunchFolder.UseVisualStyleBackColor = true;
+            this.btnLaunchFolder.Click += new System.EventHandler(this.btnLaunchFolder_Click);
+            // 
+            // btnCopyFile
+            // 
+            this.btnCopyFile.Location = new System.Drawing.Point(27, 675);
+            this.btnCopyFile.Name = "btnCopyFile";
+            this.btnCopyFile.Size = new System.Drawing.Size(75, 23);
+            this.btnCopyFile.TabIndex = 7;
+            this.btnCopyFile.Text = "Copy";
+            this.btnCopyFile.UseVisualStyleBackColor = true;
+            this.btnCopyFile.Click += new System.EventHandler(this.btnCopyFile_Click);
+            // 
+            // btnLaunchFile
+            // 
+            this.btnLaunchFile.Location = new System.Drawing.Point(108, 675);
+            this.btnLaunchFile.Name = "btnLaunchFile";
+            this.btnLaunchFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLaunchFile.TabIndex = 8;
+            this.btnLaunchFile.Text = "Launch Folder";
+            this.btnLaunchFile.UseVisualStyleBackColor = true;
+            this.btnLaunchFile.Click += new System.EventHandler(this.btnLaunchFile_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnLaunchFile);
+            this.Controls.Add(this.btnCopyFile);
+            this.Controls.Add(this.btnLaunchFolder);
+            this.Controls.Add(this.btnCopyFolder);
+            this.Controls.Add(this.lvFiles);
+            this.Controls.Add(this.lvFolders);
+            this.Controls.Add(this.btnSetFolder);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(362, 712);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSetFolder;
+        private System.Windows.Forms.ListView lvFolders;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView lvFiles;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button btnCopyFolder;
+        private System.Windows.Forms.Button btnLaunchFolder;
+        private System.Windows.Forms.Button btnCopyFile;
+        private System.Windows.Forms.Button btnLaunchFile;
+    }
+}
---
+++ src/SplitDirView/SplitDirView/UserControl1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitDirView
+{
+    public partial class UserControl1: UserControl
+    {
+
+        private NavFolders navFolders;
+        private NavFiles navFiles;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+            NavFolderSet(@"C:\Users\scott\Documents\GitHub\pyABF\data\abfs");
+        }
+
+        public event EventHandler FolderChanged;
+        protected virtual void OnFolderChanged(EventArgs e)
+        {
+            var handler = FolderChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        public event EventHandler FileSelected;
+        protected virtual void OnFileSelected(EventArgs e)
+        {
+            var handler = FileSelected;
+            if (handler != null)
+                handler(this, e);
+        }
+
+        public ListViewItem[] LabelsToListViewItems(string[] strings, int ImageIndex=0)
+        {
+            ListViewItem[] lvItems = new ListViewItem[strings.Length];
+            for (int i=0; i<strings.Length; i++)
+            {
+                ListViewItem lvItem = new ListViewItem();
+                lvItem.ImageIndex = ImageIndex;
+                if (strings[i].EndsWith("\\") || strings[i].EndsWith("/"))
+                    lvItem.ImageIndex = 0;
+                lvItem.Text = strings[i];
+                //lvItem.ForeColor = Color.Blue;
+                lvItems[i] = lvItem;
+            }
+            return lvItems;
+        }
+
+        public string currentFolder;
+        private void NavFolderSet(string folder)
+        {
+            navFolders = new NavFolders(folder);
+            lvFolders.Items.Clear();
+            lvFolders.Items.AddRange(LabelsToListViewItems(navFolders.GetLabels()));
+
+            // after populating the folder list, select the last folder
+            lvFolders.Items[lvFolders.Items.Count - 1].Selected = true;
+            lvFolders.Select();
+
+            currentFolder = folder;
+            OnFolderChanged(EventArgs.Empty);
+        }
+
+        private void btnSetFolder_Click(object sender, EventArgs e)
+        {
+            //NavFolderSet(@"C:\Users\scott\Documents\GitHub\pyABF\data\abfs");
+        }
+
+        private void lvFolders_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("some long message here", "WARNING!!!");
+
+            // figure what folder was clicked
+            if (lvFolders.SelectedIndices == null || lvFolders.SelectedIndices.Count == 0)
+                return;
+            string path = navFolders.folders[lvFolders.SelectedIndices[0]].path;
+
+            // populate the file list
+            navFiles = new NavFiles(path);
+            lvFiles.Items.Clear();
+            lvFiles.Items.AddRange(LabelsToListViewItems(navFiles.GetPathNames(), 2));
+            lvFiles.Columns[0].Width = -2;
+        }
+
+        private void lvFolders_DoubleClick(object sender, EventArgs e)
+        {
+            // figure what folder was clicked
+            if (lvFolders.SelectedIndices == null || lvFolders.SelectedIndices.Count == 0)
+                return;
+            string path = navFolders.folders[lvFolders.SelectedIndices[0]].path;
+            NavFolderSet(path);
+        }
+
+        public string highlightedFile;
+        private void lvFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // figure what folder was clicked
+            if (lvFiles.SelectedIndices == null || lvFiles.SelectedIndices.Count == 0)
+                return;
+            string path = navFiles.paths[lvFiles.SelectedIndices[0]];
+            highlightedFile = path;
+        }
+
+        public string doubleClickedFile;
+        private void lvFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // figure what folder was clicked
+            if (lvFiles.SelectedIndices == null || lvFiles.SelectedIndices.Count == 0)
+                return;
+            string path = navFiles.paths[lvFiles.SelectedIndices[0]];
+
+            // determine what to do if it's a file or folder
+            if (System.IO.Directory.Exists(path))
+            {
+                System.Console.WriteLine($"double-clicked a DIRECTORY: {path}");
+                NavFolderSet(path);
+            }
+            else
+            {
+                System.Console.WriteLine($"double-clicked a FILE: {path}");
+                doubleClickedFile = path;
+                OnFileSelected(EventArgs.Empty);
+            }
+        }
+
+        private void btnCopyFolder_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(currentFolder);
+        }
+
+        private void btnCopyFile_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(highlightedFile);
+        }
+
+        private void btnLaunchFolder_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start($"{currentFolder}");
+        }
+
+        private void btnLaunchFile_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start($"{highlightedFile}");
+        }
+    }
+}
