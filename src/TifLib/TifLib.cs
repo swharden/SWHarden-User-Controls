2c5f0286dcbae7ae8f50ed8c66398e6cf5cbda51
Scott W Harden <swharden@gmail.com>
2018-09-08 07:36:50 -0400

    started TifLib

---
+++ src/TifLib/TifLib.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TifLib
+{
+    public class TifFile
+    {
+        public TifFile(string filePath)
+        {
+
+        }
+
+        public string Info()
+        {
+            string msg = "something cool";
+            return msg;
+        }
+    }
+}
---
+++ src/TifLibDemo/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TifLib;
+
+namespace TifLibDemo
+{
+    public partial class Form1 : Form
+    {
+        public TifFile tif;
+        public Form1()
+        {
+            InitializeComponent();
+            tif = new TifFile("");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = tif.Info();
+        }
+    }
+}
